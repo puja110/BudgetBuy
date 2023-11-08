87d1ae570861a3bc16f574e4f5a7af20a5074a9d
Yogesh Neupane <neupaneyogesh97@gmail.com>
2023-11-08 16:49:04 -0500

    style change

--- Screens/MyFavourite.tsx
+++ Screens/MyFavourite.tsx
@@ -95,8 +95,7 @@ export default function MyFavourites({ navigation }){
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
-        marginTop:25
+        flex:1
     },
     headContainer:{
         marginTop: 15,
