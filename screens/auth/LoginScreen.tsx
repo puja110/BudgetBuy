327a069518f6e1f272f79d26b99e81794bfec56f
puja110 <csta.puja@gmail.com>
2023-11-10 09:43:46 -0500

    minor fixes made

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -116,6 +116,7 @@ export default function LoginScreen({ navigation }) {
                 />
               </TouchableOpacity>
             </View>
+          </View> 
         </View>
       </ImageBackground>
     </SafeAreaView>
