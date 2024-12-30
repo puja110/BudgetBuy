4f721d844ca8c5b30bf12f773d6f745ca6015461
puja110 <csta.puja@gmail.com>
2024-12-30 13:40:19 -0500

    type check minor fixes

--- src/views/components/header/ChatHeader.tsx
+++ src/views/components/header/ChatHeader.tsx
@@ -5,11 +5,9 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Image,
-  ViewStyle,
-  TextStyle,
 } from 'react-native';
 import {RFValue} from 'react-native-responsive-fontsize';
-import Icon from 'react-native-vector-icons/dist/Octicons';
+import Icon from 'react-native-vector-icons/Octicons';
 import MetaAILogo from '../../../assets/logo_s.jpeg';
 
 const BudgetBotHeader: React.FC = () => {
@@ -37,24 +35,24 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(20,25,46,1)',
     borderBottomWidth: 0.18,
     borderBottomColor: 'rgba(62,62,63,1)',
-  } as ViewStyle,
+  },
   subContainer: {
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
     gap: 10,
-  } as ViewStyle,
+  },
   logo: {
     width: 38,
     height: 38,
     borderRadius: 40,
-  } as ViewStyle,
+  },
   flexRow: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     gap: 10,
-  } as ViewStyle,
+  },
 });
 
 export default BudgetBotHeader;
