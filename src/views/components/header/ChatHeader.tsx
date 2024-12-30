5a124b9c97975fcb78f182e357caf5cfdb43ad88
puja110 <csta.puja@gmail.com>
2024-12-30 13:24:01 -0500

    Custom text component added, Type check added in CustomText vector icon types installed

--- src/views/components/header/ChatHeader.tsx
+++ src/views/components/header/ChatHeader.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import {
-  Text,
   StyleSheet,
   View,
   TouchableOpacity,
   SafeAreaView,
+  Image,
+  ViewStyle,
+  TextStyle,
 } from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
+import Icon from 'react-native-vector-icons/dist/Octicons';
+import MetaAILogo from '../../../assets/logo_s.jpeg';
 
-const BudgetBotHeader = () => {
+const BudgetBotHeader: React.FC = () => {
+  const menuBarIcon = (
+    <Icon name="three-bars" size={RFValue(20)} color="#FFFFFF" />
+  );
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <View style={styles.subContainer}>
-          <TouchableOpacity>
-            <Text>BudgetBot</Text>
-          </TouchableOpacity>
+          <TouchableOpacity>{menuBarIcon}</TouchableOpacity>
+          <View style={styles.flexRow}>
+            <Image source={MetaAILogo} style={styles.logo} />
+          </View>
         </View>
       </SafeAreaView>
     </View>
@@ -27,13 +37,24 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(20,25,46,1)',
     borderBottomWidth: 0.18,
     borderBottomColor: 'rgba(62,62,63,1)',
-  },
+  } as ViewStyle,
   subContainer: {
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
     gap: 10,
-  },
+  } as ViewStyle,
+  logo: {
+    width: 38,
+    height: 38,
+    borderRadius: 40,
+  } as ViewStyle,
+  flexRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  } as ViewStyle,
 });
 
 export default BudgetBotHeader;
